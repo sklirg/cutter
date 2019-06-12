c8e8a50843e8681dc2c8ec753a89662a0a309876
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-06-12 19:26:43 +0200

    feat: Create multiple crops instead of just a single 'thumb' crop.

--- src/cutter/lib.rs
+++ src/cutter/lib.rs
@@ -222,36 +222,52 @@ fn transform_images(files: Vec<String>, output_path: &str) -> Vec<String> {
     let numfiles = files.len().to_owned();
     println!("Processing {} files", numfiles);
 
+    let sizes = vec![
+        // Thumbs
+        vec![200, 200],
+        vec![400, 400],
+        vec![800, 800],
+        // Full size preview
+        vec![1920, 1080],
+    ];
+
     let mut created_files = Vec::new();
 
     let mut counter = 0;
     for f in files {
         print_list_iter_status(counter, numfiles as u32, "Processed");
-        let thumb_path = format!(
-            "{}/{}",
-            output_path,
-            generate_thumb_path(&get_file_name(&f), "jpg")
-        );
-        let mut image = raster::open(&f).unwrap();
-        transform_image(&mut image);
-        save_image(&image, &thumb_path);
-        created_files.push(thumb_path);
+        for size in &sizes {
+            let width = size[0];
+            let height = size[1];
+            let image = transform_image(&f, width, height);
+
+            let thumb_path = format!(
+                "{}/{}",
+                output_path,
+                generate_thumb_path(&get_file_name(&f), width, height, "jpg")
+            );
+
+            save_image(&image, &thumb_path);
+            created_files.push(thumb_path);
+        }
         counter += 1;
     }
 
     return created_files;
 }
 
-fn transform_image(image: &mut raster::Image) {
-    raster::transform::resize_fill(image, 200, 200).unwrap();
+fn transform_image(path: &str, width: i32, height: i32) -> raster::Image {
+    let mut image = raster::open(path).unwrap();
+    raster::transform::resize_fill(&mut image, width, height).unwrap();
+    return image;
 }
 
 fn save_image(image: &raster::Image, path: &str) {
     raster::save(&image, &path).unwrap();
 }
 
-fn generate_thumb_path(path: &str, path_suffix: &str) -> String {
-    return format!("{}_thumb.{}", path, path_suffix);
+fn generate_thumb_path(path: &str, w: i32, h: i32, path_suffix: &str) -> String {
+    return format!("{}_{}x{}px_{}w.{}", path, w, h, w, path_suffix);
 }
 
 // @ToDo: Skip if not .jpg
